c5bafe3d968c35ae6599cbb65f0ac50dcdd720fe
ftbyu <ftb.y.u.kashima.07@outlook.jp>
2020-06-16 15:20:14 +0900

    Fix array

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ function randomValueFromArray(array){
 
 let storyText =  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertX = "Willy the GoblinBig Daddy Father Christmas";
+let insertX = ["Willy the GoblinBig", "Daddy", "Father Christmas"];
 
-let insertY = "the soup kitchen Disneyland the White House";
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-let insertZ = "spontaneously combusted melted into a puddle on the sidewalk turned into a slug and crawled away";
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 randomize.addEventListener('click', result);
